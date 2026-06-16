29ce924eabdb92bbb72fc5c81c4daa30f0fc8a4b
745590 <abhishekchhatraliya@gmail.com>
2026-06-16 22:01:07 +0530

    It is the day 5 of improve my python skill

---
+++ day_5.py
@@ -0,0 +1,14 @@
+# String slicing
+
+name = "Abhishek Kumar"
+nameLen = len(name)
+print(nameLen)
+print(name[0:4]) #including 0 but not 4
+print(name[4:8]) #including 4 but not 8
+print(name[:5])
+print(name[:])
+print(name[0:-3])
+print(name[:len(name) -3])
+print(name[-1:len(name) -3])
+print(name[-3:-1])
+
