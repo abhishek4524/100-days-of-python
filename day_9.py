139a7a28f3e1266d7ff3858f60f259bb01ed1bba
745590 <abhishekchhatraliya@gmail.com>
2026-06-19 06:43:22 +0530

    This is day 9 of python language learning

---
+++ day_9.py
@@ -0,0 +1,15 @@
+name = "Abhishek"
+for i in name :
+    print(i)
+
+colors = ["Red", "Green", "Blue", "Yellow"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+
+for i in range(0,50):
+    print(i + 1, end=" ")
+
+for i in range(0, 10, 2):
+    print(i)
