88fb57b8dc021d9cead12bbf1876807ea6d9522a
745590 <abhishekchhatraliya@gmail.com>
2026-06-19 22:51:23 +0530

    This is day 9 of python language learning

--- day_9.py
+++ day_9.py
@@ -1,4 +1,8 @@
+#Loops in python 
 name = "Abhishek"
+
+#For loop in python
+
 for i in name :
     print(i)
 
@@ -13,3 +17,19 @@ for i in range(0,50):
 
 for i in range(0, 10, 2):
     print(i)
+
+#while loop in python
+
+while i <= 10:
+    i = int(input("Enter a number:"))
+    print(i)
+print("Done with the loop")
+
+count = 5
+while count > 0:
+    print(count)
+    count -= 1
+else:
+    print("Count is zero now")
+
+
