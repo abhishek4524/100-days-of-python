2356754493b2aebef35f24e5556624ff19e0cd0e
745590 <abhishekchhatraliya@gmail.com>
2026-06-20 12:56:26 +0530

    This is day 10 of python language learning

---
+++ day_10.py
@@ -0,0 +1,15 @@
+# break statement in python
+
+for i in range(5):
+    print(i)
+    if i == 3:
+        break
+
+print("break statment program end")
+
+#continue statement in python 
+for i in range(10):
+    print(i)
+    if (i == 4):
+        continue 
+print("continue statment program end")
