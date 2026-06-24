02d3e1a62a1bb10cdea2f56a6948b0377d8aadd3
745590 <abhishekchhatraliya@gmail.com>
2026-06-24 22:21:34 +0530

    This is day 14 of python language learning

---
+++ day_14.py
@@ -0,0 +1,24 @@
+#List Manupulation
+l = [ 11, 2 , 45 , 16]
+print(l)
+# l.append(7)
+# print(l)
+
+# l.sort()
+# print(l)
+# l.sort(reverse=True)
+# print(l)
+# l.reverse()
+# print(l)
+# print(l.index(45))
+# print(l.count(45))
+
+# m = l.copy()
+# m[0] =  0
+# l.insert(1, 345)
+
+m = [96, 656, 78, 89]
+k = l + m
+# l.extend(m)
+print(k)
+print(l)
